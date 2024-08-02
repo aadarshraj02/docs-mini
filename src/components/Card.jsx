ce34ff742289969125bf6043420b20f3fef0cdd7
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-02 18:07:41 +0530

    added file icon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,11 @@
+import { FaRegFileAlt } from "react-icons/fa";
+
 const Card = () => {
   return (
     <div>
-      <div className="w-60 h-72 rounded-lg bg-zinc-900/90"></div>
+      <div className="w-60 h-72 rounded-lg bg-zinc-900/90 text-white">
+        <FaRegFileAlt />
+      </div>
     </div>
   );
 };
