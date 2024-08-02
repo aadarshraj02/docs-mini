ce4b678190d24a82117ea3b7fc1140b618e6b496
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-02 17:55:34 +0530

    added div with width and height and fixed to top

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,5 +1,9 @@
 const Foreground = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="fixed top-0 left-0 z-20 w-full h-full"></div>
+    </div>
+  );
 };
 
 export default Foreground;
