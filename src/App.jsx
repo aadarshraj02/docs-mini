103d0cb7574f97b3e4f32edc30e667e9d676d4ce
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-02 17:37:12 +0530

    removed code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
 const App = () => {
-  return (
-    <div className="relative w-full h-screen bg-zinc-800">
-      <div className="w-full py-10 flex justify-center text-zinc-500 font-semibold text-xl absolute top[5%]">
-        Documents.
-      </div>
-      <h1 className="absolute text-[13vw] leading-none tracking-tighter text-zinc-900 font-semibold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        Docs
-      </h1>
-    </div>
-  );
+  return <div className="relative w-full h-screen bg-zinc-800"></div>;
 };
 
 export default App;
