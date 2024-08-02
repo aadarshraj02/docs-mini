3942f5d20c2e2acf6495d1846cecea22ba2bb727
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-02 17:17:58 +0530

    styled h1; placed in center updated font typography

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
-    <div className="w-full h-screen bg-zinc-800">
-      <h1>Docs</h1>
+    <div className="relative w-full h-screen bg-zinc-800">
+      <h1 className="absolute text-[13vw] leading-none tracking-tighter text-zinc-900 font-semibold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        Docs
+      </h1>
     </div>
   );
 };
