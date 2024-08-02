668587a557c11a0cb9438a5447982935eeef8efb
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-02 18:13:17 +0530

    updated padding

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import { FaRegFileAlt } from "react-icons/fa";
 const Card = () => {
   return (
     <div>
-      <div className="relative w-60 h-72 rounded-lg p-5 bg-zinc-900/90 text-white overflow-hidden">
+      <div className="relative w-60 h-72 rounded-lg px-5 py-10 bg-zinc-900/90 text-white overflow-hidden">
         <FaRegFileAlt />
         <p className="text-sm mt-5 font-semibold leading-tight">
           Lorem ipsum dolor sit amet consectetur adipisicing.
