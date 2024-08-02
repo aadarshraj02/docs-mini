2bdfa49ed58db2597a8daf88879526676d0352ad
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-02 19:26:52 +0530

    added data

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,6 +1,20 @@
+// import { useState } from "react";
 import Card from "./Card";
 
 const Foreground = () => {
+  const data = [
+    {
+      desc: "  Lorem ipsum dolor sit amet consectetur adipisicing.",
+      fileSize: "0.8mb",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+  ];
+
   return (
     <div>
       <div className="fixed top-0 left-0 z-20 w-full h-full">
