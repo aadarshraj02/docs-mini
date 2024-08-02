bf6b9ac4c69e4cb78294e0f29a3a18ed2f4d76d1
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-02 18:31:48 +0530

    updated icon size and with and height of span

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,10 +10,12 @@ const Card = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing.
         </p>
 
-        <div className="absolute bottom-0 left-0 w-full py-3 bg-sky-200">
-          <div>
+        <div className="absolute bottom-0 left-0 w-full py-3 px-8 ">
+          <div className="flex items-center justify-between mb-5">
             <h5>.4mb</h5>
-            <LuDownload />
+            <span className="w-7 h-7 rounded-full flex items-center justify-center bg-zinc-600">
+              <LuDownload size=".7em" className="text-white" />
+            </span>
           </div>
         </div>
       </div>
