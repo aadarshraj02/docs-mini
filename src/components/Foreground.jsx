0f96913b0ba92c01ebf31fa5e8ca39848766c195
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-02 18:01:26 +0530

    imported card

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,7 +1,11 @@
+import Card from "./Card";
+
 const Foreground = () => {
   return (
     <div>
-      <div className="fixed top-0 left-0 z-20 w-full h-full"></div>
+      <div className="fixed top-0 left-0 z-20 w-full h-full">
+        <Card />
+      </div>
     </div>
   );
 };
