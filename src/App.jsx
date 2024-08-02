2feb1fa770ce1112b6f4c5f91796e3f29f8c84f8
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-02 17:32:10 +0530

    added div for showing header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 const App = () => {
   return (
     <div className="relative w-full h-screen bg-zinc-800">
+      <div className="w-full py-10 flex justify-center text-zinc-700 font-semibold">
+        Documents.
+      </div>
       <h1 className="absolute text-[13vw] leading-none tracking-tighter text-zinc-900 font-semibold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         Docs
       </h1>
