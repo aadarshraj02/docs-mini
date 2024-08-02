0c1ceb3376ef59cd387db540322037ef6c9b7f29
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-02 19:42:36 +0530

    added condotions when to show data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,30 @@
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
-const Card = () => {
+const Card = ({ data }) => {
   return (
     <div>
       <div className="relative w-60 h-72 rounded-[30px] px-5 py-10 bg-zinc-900/90 text-white overflow-hidden">
         <FaRegFileAlt />
-        <p className="text-sm mt-5 font-semibold leading-tight">
-          Lorem ipsum dolor sit amet consectetur adipisicing.
-        </p>
+        <p className="text-sm mt-5 font-semibold leading-tight">{data.desc}</p>
 
         <div className="absolute bottom-0 left-0 w-full">
           <div className="flex items-center justify-between mb-3 px-8  py-3">
-            <h5>.4mb</h5>
+            <h5>{data.fileSize}</h5>
             <span className="w-7 h-7 rounded-full flex items-center justify-center bg-zinc-600">
-              <LuDownload size=".7em" className="text-white" />
+              {data.close ? (
+                <LuDownload size=".7em" className="text-white" />
+              ) : (
+                <IoMdCloseCircleOutline size=".7em" className="text-white" />
+              )}
             </span>
           </div>
-          <div className="w-full py-4 bg-green-600 flex items-center justify-center">
-            <h3 className="text-sm font-semibold">Download Now</h3>
-          </div>
+          {data.tag.isOpen ? (
+            <div className="w-full py-4 bg-green-600 flex items-center justify-center">
+              <h3 className="text-sm font-semibold">Download Now</h3>
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
