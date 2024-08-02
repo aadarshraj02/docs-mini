ef14fbe2a7955a079e333f4e1c7ee09e66e1b9e9
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-02 18:10:19 +0530

    added p; styled and made div relative

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,11 @@ import { FaRegFileAlt } from "react-icons/fa";
 const Card = () => {
   return (
     <div>
-      <div className="w-60 h-72 rounded-lg bg-zinc-900/90 text-white">
+      <div className="relative w-60 h-72 rounded-lg p-5 bg-zinc-900/90 text-white">
         <FaRegFileAlt />
+        <p className="text-sm mt-5 font-semibold leading-tight">
+          Lorem ipsum dolor sit amet consectetur adipisicing.
+        </p>
       </div>
     </div>
   );
