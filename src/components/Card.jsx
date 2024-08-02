86fe2bf6d3174e7a52cdf30a2247a8f26255b958
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-02 18:37:59 +0530

    added h3 styled it and updated padding

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,12 +10,15 @@ const Card = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing.
         </p>
 
-        <div className="absolute bottom-0 left-0 w-full py-3 px-8 ">
-          <div className="flex items-center justify-between mb-5">
+        <div className="absolute bottom-0 left-0 w-full">
+          <div className="flex items-center justify-between mb-3 px-8  py-3">
             <h5>.4mb</h5>
             <span className="w-7 h-7 rounded-full flex items-center justify-center bg-zinc-600">
               <LuDownload size=".7em" className="text-white" />
             </span>
+          </div>
+          <div className="w-full py-4 bg-green-600 flex items-center justify-center">
+            <h3 className="text-sm font-semibold">Download Now</h3>
           </div>
         </div>
       </div>
